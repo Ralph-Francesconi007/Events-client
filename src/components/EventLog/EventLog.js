84bcaa81a6785167e98b1c407e61037a67bcb449
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-10-27 14:15:29 -0400

    Made some changes to the Event log
    I got all of the events that were created to render onto the screen
    under the Events log Component.

--- src/components/EventLog/EventLog.js
+++ src/components/EventLog/EventLog.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import apiUrl from './../../apiConfig'
 
 class EventLog extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      eventLog: []
+    }
+  }
+  componentDidMount () {
+    axios({
+      url: `${apiUrl}/event`,
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + `${this.props.user.token}`
+      }
+    })
+      .then(response => {
+        this.setState({
+          isLoaded: true,
+          eventLog: response.data.event
+        })
+      })
+      .catch(console.error)
+  }
   render () {
+    let jsx
+    if (this.state.isLoaded === false) {
+      jsx = <p>Loading ...</p>
+    } else if (this.state.eventLog === 0) {
+      jsx = <p>No Events, please add an event!</p>
+    } else {
+      jsx = (
+        <div>
+          {this.state.eventLog.map(eventLog => {
+            return (
+              <div key={eventLog._id}>
+                <h3>{eventLog.title}</h3>
+                <p>{eventLog.time}</p>
+                <p>{eventLog.date}</p>
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
     return (
-      <div>This is all the events</div>
+      <div>
+        <h2>Event Log</h2>
+        {jsx}
+      </div>
     )
   }
 }
